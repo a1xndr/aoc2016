070751416ce33acfb01cee612d23ef7874f6378d
Alexander Oleinik <alxndr@alxndr-mbp>
2016-12-05 13:33:39 -0500

    d3b done

--- day3/triangles_b.py
+++ day3/triangles_b.py
@@ -1,23 +1,17 @@
-import sys
-import re
-import numpy as np
-
-def main():
-    count = 0 
-    f = open("input" , "r")
-    cols = list()
-    for i in range(0, 2):
-        cols.append(list())
-    for line in f:
-        line = list(line.rstrip().split())
-        for i in range(0, 2):
-            cols[i].append(int(line[i]))
-            if(len(cols[i]) >= 3):
-                print("D: " + str(sorted(cols[i])) )
-                if sorted(cols[i])[0] + sorted(cols[i])[1] > sorted(cols[i])[2] :
-                    print(str(cols[i][0]) +" "+ str(cols[i][1])+" " + str(cols[i][2]) )
-                    count += 1
-                cols[i] = cols[i].pop(0)
-    print(count)
-if __name__ == "__main__":
-    main()
+f = open("input" , "r")
+cols = list()
+n = 0;
+count = 0 
+for i in range(0, 3):
+    cols.append(list())
+for line in f:
+    n+=1
+    line = list(line.rstrip().split())
+    for i in range(0, 3):
+        cols[i].append(int(line[i]))
+        if(n%3 == 0):
+            if sorted(cols[i])[0] + sorted(cols[i])[1] > sorted(cols[i])[2] :
+                count += 1
+        if(n>=3):
+            cols[i].pop(0)
+print(count)
