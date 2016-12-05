8f04259a4580daaa40a01dfde2ba09e13e4a1f89
Alexander Oleinik <alxndr@solo.vrmnet>
2016-12-05 13:17:23 -0500

    modified triangles_a

--- day3/triangles_a.py
+++ day3/triangles_a.py
@@ -5,6 +5,7 @@ import numpy as np
 def main():
     count = 0 
     f = open("input" , "r")
+
     for line in f:
         s = sorted([int(i) for i in list(line.rstrip().split())])
         if s[0] + s[1] > s[2] :
