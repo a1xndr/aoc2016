2f08e85b5689226b68be75f74944e39d178a94f4
Alexander Oleinik <alxndr@alxndr-mbp>
2016-12-19 15:37:28 -0500

    much faster bitwise c version

--- day18/traps-a.py
+++ day18/traps-a.py
@@ -1,15 +1,23 @@
-input = ".^^.^.^^^^"
-rows = 10
+input= ".^^.^.^^^^"
+rows = 9
 safe = 0
 prevrow = list()
 for c in input:
-    prevrow.append(0 if c = "." else 1)
+    prevrow.append(0 if c == "." else 1)
 safe = prevrow.count(0)
 
-for n in range(0, rows):
+for n in range(0, rows-1):
+    row = list()
     for i in range(0, len(prevrow)):
         l = prevrow[i-1] if i >0 else 0
         c = prevrow[i]
         r = prevrow[i+1] if i < len(prevrow)-1 else 0
-        row = prevrow
+        row.append((c and ( l ^ r)) or ( (not c) and (l ^ r)))
+    prevrow = row
+    safe += prevrow.count(0)
+    for c in row:
+        print('^' if c else '.', end='')
+    print()
+print(safe)
+
     
